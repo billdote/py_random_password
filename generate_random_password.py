2c00a1b03c1535b335e123350cb56b0023df36f4
billdote <billy@thebp.io>
2022-09-17 01:15:19 -0400

    create or append to a password file to keep track of logins if desired

--- generate_random_password.py
+++ generate_random_password.py
@@ -1,15 +1,19 @@
+from csv import writer
 import string
 import random
+import re
+import os
 import sys
+
 
 def generate_random_password():
     length = int(input('Enter password length: '))
-    
+
     exclude_schar = input('Enter special characters to exclude (!@#$()%^&*): ')
     schar = '!@#$%^&*()'.replace(exclude_schar, '') if exclude_schar else '!@#$%^&*()'
 
     characters = list(string.ascii_letters + string.digits + schar)
-    
+
     random.shuffle(characters)
     password = []
 
@@ -17,8 +21,36 @@ def generate_random_password():
         password.append(random.choice(characters))
 
     random.shuffle(password)
-    print(f"\nGenerated Password: {''.join(password)}")
+    password = ''.join(password)
 
+    create_file = input(
+        'Do you want to create or add to a password file? (Y/N) ')
+
+    if re.match('^Y|y$', create_file):
+        create_password_file(password)
+    else:
+        print(f"\nGenerated Password: {password}")
+
+
+def create_password_file(password):
+    # file headers
+    col_names = ['USERNAME', 'PASSWORD', 'DESCRIPTION']
+
+    username = input('\nEnter username: ')
+    description = input('Enter description (what the login is for): ')
+
+    data = [username, password, description]
+
+    # create a pipe delimited password text file or append to it
+    with open('passwords.csv', 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+
+        if os.stat('passwords.csv').st_size == 0:
+            csv_writer.writerow(col_names)
+        csv_writer.writerow(data)
+
+
+# ignore errors and exit
 try:
     generate_random_password()
 except:
